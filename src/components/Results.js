959ca6b59949d7b91b36b1217f85352766333341
S.Haturusinghe <haturusingh3@gmail.com>
2020-12-10 14:17:34 +0530

    more styling

--- src/components/Results.js
+++ src/components/Results.js
@@ -4,7 +4,7 @@ import DataContext from "../contexts/DataContext";
 import "../styles/result.css";
 
 import ResultTable from "./ResultTable";
-import { Grid } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 
 class Results extends Component {
   // Update the Context. If you recall from Step 2, this is
@@ -24,14 +24,28 @@ class Results extends Component {
         style={{
           margin: "5px",
           width: "99%",
+
           backgroundColor: "white",
           border: "2px solid",
           borderColor: "#E7EDF3",
         }}
       >
-        {this.context.data.map((item, index) => {
-          return <ResultTable key={index} result={item} />;
-        })}
+        {this.context.data.length == 0 ? (
+          <Grid container direction="row" justify="center" alignItems="stretch">
+            <Typography
+              variant="h4"
+              gutterBottom
+              className="inputLabelStyle"
+              style={{ padding: "5px", marginTop: 0 }}
+            >
+              Please provide at least one search parameter
+            </Typography>
+          </Grid>
+        ) : (
+          this.context.data.map((item, index) => {
+            return <ResultTable key={index} result={item} />;
+          })
+        )}
       </Grid>
     );
   }
