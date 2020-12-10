300f4be15929883c22fe6e6aa532b3177196ce2c
S.Haturusinghe <haturusingh3@gmail.com>
2020-12-10 13:37:46 +0530

    changed map onLoad bounds

--- src/components/MapV3.js
+++ src/components/MapV3.js
@@ -11,9 +11,8 @@ const containerStyle = {
 
 let myPlaces = [];
 const myPlaces2 = [
-  { id: "place1", pos: { lat: 39.09366509575983, lng: -94.58751660204751 } },
-  { id: "place2", pos: { lat: 39.10894664788252, lng: -94.57926449532226 } },
-  { id: "place3", pos: { lat: 39.07602397235644, lng: -94.5184089401211 } },
+  { id: "colombo", pos: { lat: 6.93197, lng: 79.85775 } },
+  { id: "kalutara", pos: { lat: 6.58084, lng: 79.96293 } },
 ];
 
 const options = {
